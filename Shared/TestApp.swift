e57adf5cb90020ec0e7b1166c71cc4e21498defc
lushuang <lushuang@igetcool.com>
2021-09-01 16:54:01 +0800

    add test

---
+++ Shared/TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestApp.swift
+//  Shared
+//
+//  Created by lushuang on 2021/9/1.
+//
+
+import SwiftUI
+
+@main
+struct TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
